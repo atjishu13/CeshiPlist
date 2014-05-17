778e703a0788730061fd4629e62ab1a6999a60c9
johnchen <chenxipeng-08@163.com>
2014-05-17 10:32:26 +0800

    第一次上传
    测试

--- Ceshi/ATViewController.m
+++ Ceshi/ATViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ATViewController.h"
-
+#import "UIDevice+IdentifierAddition.h"
 @interface ATViewController ()
 
 @end
@@ -18,6 +18,23 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(10, 100, 300, 100)];
+    label.numberOfLines = 0;
+    label.textAlignment = NSTextAlignmentCenter;
+    label.text = [NSString stringWithFormat:@"Unique Device Identifier:\n%@",
+                  [[UIDevice currentDevice] uniqueDeviceIdentifier]];
+    
+    [self.view addSubview:label];
+//    [label release];
+    
+    label = [[UILabel alloc] initWithFrame:CGRectMake(10, 210, 300, 100)];
+    label.numberOfLines = 0;
+    label.textAlignment = NSTextAlignmentCenter;
+    label.text = [NSString stringWithFormat:@"Unique GLOBAL Device Identifier:\n%@",
+                  [[UIDevice currentDevice] uniqueGlobalDeviceIdentifier]];
+    
+    [self.view addSubview:label];
+//    [label release];
 }
 
 - (void)didReceiveMemoryWarning
